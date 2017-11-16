c01e487a2c376288b7395b6f1bad8a034f67e88b
githubuseronline <kilomaymany@rambler.ru>
2017-11-16 14:55:12 +0200

    Add files via upload

--- proxy.py
+++ proxy.py
@@ -5,13 +5,22 @@ from optparse import OptionParser, OptionGroup
 
 if __name__ == '__main__':
 
-    parser = OptionParser()
-    parser.add_option('-i', '--interface', action='store', default='127.0.0.1',
-                                           help='interface for listening', 
-                                           metavar='[interface]')
-#    parser.add_option('-p', '--port', )
+    ################################################################################
+
+    # Parse command line args and options
+
+    ################################################################################
+    parser = OptionParser(description='DNS Proxy.')
+
+    serv_opts = OptionGroup(parser, 'Basic options for proxy.')
+    serv_opts.add_option('-i', '--interface', default='127.0.0.1',
+                         help='Interface for proxy. By default is %default')
+    serv_opts.add_option('-p', '--port', default='53',
+                         help='Proxy port. By default is 53')
+    serv_opts.add_option('-t', '--tcp', action='store_true', default='False',
+                         dest='transport', help='UDP is a default transport.')
+    parser.add_option_group(serv_opts)
 
     (options, args) = parser.parse_args()
 
-    print(options.interface)
-    print(options.interface)
+    print(options.transport)
