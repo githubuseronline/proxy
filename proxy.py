1db7d5e832e7f976d1b093825eae20618602712b
githubuseronline <kilomaymany@rambler.ru>
2017-11-16 13:25:17 +0200

    Add files via upload

---
+++ proxy.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from optparse import OptionParser, OptionGroup
+
+
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option('-i', '--interface', action='store', default='127.0.0.1',
+                      help='interface for listening', metavar='[interface]')
+#    parser.add_option('-p', '--port', )
+
+    (options, args) = parser.parse_args()
+
+    print(options.interface)
+    print(options.interface)
